b90d9584a058fd0e77255f5232e820645111b24e
Ranit Bhowmick <104001870+Kawai-Senpai@users.noreply.github.com>
2025-02-09 22:50:12 +0530

    Update Docker configuration for testing environment and refine ignore files

--- _run.windows.bat
+++ _run.windows.bat
@@ -1,1 +1,1 @@
-docker run -p 8000:8000 --add-host=host.docker.internal:host-gateway infinite-aiml
+docker run -p 8000:8000 --env ENVIRONMENT=testing --env-file .env.testing --add-host=host.docker.internal:host-gateway infinite-aiml
